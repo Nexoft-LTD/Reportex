efd45cc432706495834c69276200609d5e236a92
turalibrahimov <tibrahimov@std.beu.edu.az>
2022-06-30 16:33:50 +0400

    First commit

---
+++ src/test/java/com/reportex/NexoftApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.nexoft;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class NexoftApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
