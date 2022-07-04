c71f00f33a701006d1e4d9324e910462e6c5a19f
Tural İbrahimov <tibrahimov@std.beu.edu.az>
2022-07-04 13:04:58 +0400

    Test commit/push

--- src/main/java/com/reportex/Controller/MessageController.java
+++ src/main/java/com/reportex/Controller/MessageController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
     @GetMapping
     public ResponseEntity<String> getMessage() {
-        return ResponseEntity.ok("Merhaba JWT");
+        return ResponseEntity.ok("Salam JWT");
     }
 }
