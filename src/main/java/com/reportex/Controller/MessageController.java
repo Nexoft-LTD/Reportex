bcc4e61393c0f02a74fce5cd0e86d7b66f9df4bb
Tural İbrahimov <tibrahimov@std.beu.edu.az>
2022-07-04 18:20:24 +0400

    add logging spring boot

--- src/main/java/com/reportex/Controller/MessageController.java
+++ src/main/java/com/reportex/Controller/MessageController.java
@@ -4,11 +4,17 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @RestController
 @RequestMapping("/message")
 @Api(value = "First api", description = "Testlerin aparilmasi ucun nezerde tutulmus yer")
@@ -23,7 +29,13 @@ public class MessageController {
     }
     )
     @GetMapping
-    public ResponseEntity<String> getMessage() {
-        return ResponseEntity.ok("Salam JWT");
+    public ResponseEntity<Integer> getMessage(@RequestParam String testData) {
+        try {
+            Integer test = Integer.parseInt(testData);
+            return ResponseEntity.ok(test);
+        } catch (Exception e) {
+            log.error("Error : {} ", e.getMessage());
+        }
+        return null;
     }
 }
